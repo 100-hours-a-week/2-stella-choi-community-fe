a3f75a563fb5004c369f0d873763265433392fb7
choiyoorim <yulim3839@naver.com>
2024-12-28 12:15:18 +0900

    bug: 🐛 비밀번호 유효성 검사 중 특수문자 범위 추가

--- public/editpassword/editpassword.js
+++ public/editpassword/editpassword.js
@@ -50,8 +50,8 @@ function showToast(message) {
 }
 
 const validatePassword = (password) => {
-    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
-    return re.test(password);
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^\w\s])[A-Za-z\d@$!%*?&^#(){}[\]<>.,~`+=:;|'"\-_\\\/]{8,}$/;
+    return passwordRegex.test(password);
 };
 
 const validatePasswordMatch = () => {
